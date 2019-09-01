13c1dccf920af656a7c59c55d7b5dc5bf8b69261
infoey <32243391+infoey@users.noreply.github.com>
2019-09-01 09:30:02 +0100

    Second Commit
    Second Commit

--- app/src/main/java/com/infoey/vodanh/databaseinfoey/MainActivity.java
+++ app/src/main/java/com/infoey/vodanh/databaseinfoey/MainActivity.java
@@ -3,6 +3,7 @@ package com.infoey.vodanh.databaseinfoey;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
